c52d2af034ac02a1c57eb7d57f6e8e4a49500196
sone <sthaya@gmail.com>
2023-03-19 20:06:09 -0700

    setup db context

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //  NOT USING THESE SERVICES, BUT ILL LEAVE THEM HERE FOR NOW
 // builder.Services.AddEndpointsApiExplorer();
